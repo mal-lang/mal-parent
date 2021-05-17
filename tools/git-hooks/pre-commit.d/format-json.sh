e7cf9f878e75b5b8ed965a04fb63dc1856adaf6f
Max Wällstedt <max.wallstedt@foreseeti.com>
2021-05-17 19:21:13 +0200

    Update copyright year and versions

--- tools/git-hooks/pre-commit.d/format-json.sh
+++ tools/git-hooks/pre-commit.d/format-json.sh
@@ -4,20 +4,32 @@ set -eu
 
 cd "$(dirname "$0")/../../.."
 
-set +e
-changed_json="$(
-  git diff --cached --name-only --diff-filter=ACMR |
-  grep "\.json$"
-)"
-set -e
+echo_changed_json() {
+  set +e
+  git diff --cached --name-only --diff-filter=ACMR | grep "\.json$"
+  set -e
+}
 
-if [ -z "$changed_json" ]; then
-  exit
-fi
+format_json() {
+  echo "Formatting $(echo "$changed_json" | wc -l) json files"
+  echo "$changed_json" | while IFS= read -r file; do
+    if jq empty "$file" >/dev/null 2>&1; then
+      # shellcheck disable=SC2005
+      echo "$(jq . "$file")" >"$file"
+    else
+      >&2 printf "%s: %s\n" "$file" "$(jq empty "$file" 2>&1)"
+      exit 1
+    fi
+  done
+  echo "$changed_json" | tr "\n" "\0" | xargs -0 git add -f
+}
 
-echo "Formatting $(echo "$changed_json" | wc -l) json files"
-echo "$changed_json" | while IFS= read -r file; do
-  # shellcheck disable=SC2005
-  echo "$(jq . "$file")" >"$file"
-done
-echo "$changed_json" | tr "\n" "\0" | xargs -0 git add -f
+main() {
+  changed_json="$(echo_changed_json)"
+  if [ -z "$changed_json" ]; then
+    exit
+  fi
+  format_json
+}
+
+main
